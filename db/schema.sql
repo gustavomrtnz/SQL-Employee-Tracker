29d93d8b7b5e030fc4572e45c7cfe3f055c08fa5
Gustavo Martinez <gustavo.mrtnz99@gmail.com>
2024-09-24 17:57:14 -0700

    fixed issue to update seeds table

--- db/schema.sql
+++ db/schema.sql
@@ -5,31 +5,33 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 --TODO: add syntax to use databse
-\c employee_db
+\c employee_db;
 
 --TODO: create tables for department, role and employee
 CREATE TABLE departments (
-    department_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
+
 );
 
 CREATE TABLE roles (
-    role_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+    department_id INTEGER NOT NULL, 
+    FOREIGN KEY(department_id) 
+    REFERENCES departments(id)
+
 );
 
-
-
 CREATE TABLE employees (
-    employee_id SERIAL PRIMARY KEY, 
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    role_id INTEGER NOT NULL, 
     manager_id INTEGER,
-    FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+    FOREIGN KEY(role_id)
+    REFERENCES roles(id)
+    
 );
 --TODO: add FOREIGN KEYS and REFERENCES to implenent other table
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
 --TODO: create in formation to be inserted into tables from schema.sql
 INSERT INTO departments (name)
-VALUES  ("Engineering"),
-        ("Finance"),
-        ("Sales"),
-        ("Legal");
+VALUES  ('Engineering'),
+        ('Finance'),
+        ('Sales'),
+        ('Legal');
 
 INSERT INTO roles (title, salary,department_id)
-VALUES  ("Software Engineer", 80000, 1),
-        ("Project Manager", 100000, 1),
-        ("Accountant", 70000, 2),
-        ("Financial Analyst", 85000, 2),
-        ("Sales Representative", 60000, 3),
-        ("Marketing Manager", 80000, 3),
-        ("Legal Advisor", 90000, 4),
-        ("Head of Legal", 120000, 4);
+VALUES  ('Software Engineer', 80000, 1),
+        ('Project Manager', 100000, 1),
+        ('Accountant', 70000, 2),
+        ('Financial Analyst', 85000, 2),
+        ('Sales Representative', 60000, 3),
+        ('Marketing Manager', 80000, 3),
+        ('Legal Advisor', 90000, 4),
+        ('Head of Legal', 120000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES  ("John", "Doe", 1, NULL),
-        ("Jane", "Smith", 2, 1),
-        ("Michael", "Johnson", 3, 2),
-        ("Sarah", "Williams", 4, 3),
-        ("David", "Brown", 5, 4),
-        ("Samantha", "Jones", 6, 5),
-        ("David", "Garcia", 7, 6),
-        ("Emily", "Davis", 8, NULL);
+VALUES  ('John', 'Doe', 1, NULL),
+        ('Jane', 'Smith', 2, 1),
+        ('Michael', 'Johnson', 3, 2),
+        ('Sarah', 'Williams', 4, 3),
+        ('David', 'Brown', 5, 4),
+        ('Samantha', 'Jones', 6, 5),
+        ('David', 'Garcia', 7, 6),
+        ('Emily', 'Davis', 8, NULL);
